77bcd75f33a88581cb6e1549c296a89bc6910e07
maccabee <maccabeeodin@gmail.com>
2019-02-08 07:05:02 +0200

    Header Styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header/>
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
-}
+};
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.logo {
+  text-align: center;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <h1>Blog Name</h1>
+    <nav>
+      <ul>
+        <li>
+          <a href>Home</a>
+        </li>
+        <li>
+          <a href>About</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  font-size: 1.1em;
+  background-color: #575757;
+  color: #bbbbbb;
+  align-content: center;
+}
+h1 {
+  display: inline;
+  padding: 0px 20px;
+}
+nav {
+  display: flex;
+  align-content: center;
+}
+ul {
+  display: inline-flex;
+}
+li {
+  display: inline;
+  padding: 20px 10px;
+}
+nav ul li a {
+  color: #bbbbbb;
+  text-decoration: none;
+}
+</style>
